f8cadd467c9906ba0afec011c973bc1c45832aea
Jáchym Dvořák <jachym.dvorak@seznam.cz>
2022-04-06 09:43:49 +0200

    changed names of plots and generation process

--- components/analyzer/analyzer.py
+++ components/analyzer/analyzer.py
@@ -1,4 +1,3 @@
-# import knihoven
 from sklearn.preprocessing import MultiLabelBinarizer
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,18 +9,18 @@ raw = pd.read_csv('data/mushrooms_dataset.csv', index_col=0)
 # Helper functions
 ###############################################################################
 
-def change_dash_to_nan(cell):
+def change_dash_to_nan(value: str) -> str:
     '''
-    zmeni "-" v bunce na nema
+    changes "-" to "nemá"
     '''
     
-    if cell == '-':
-        cell = 'nemá'
-    return cell
+    if value == '-':
+        value = 'nemá'
+    return value
 
-def to_list(value):
+def to_list(value: str) -> list:
     '''
-    ziska ze seznamu mesicu a mist vyskytu jednotlive mesice/mista 
+    changes a large string of values into a list of values
     '''
     list_of_vals = value.split(', ')
     return list_of_vals
@@ -31,74 +30,74 @@ def to_list(value):
 ###############################################################################
 
 ############
-# prvni graf
+# (1) plot
 ############
-# prekodovat binarni sloupce
+# recode binary columns
 raw['pochva'] = raw['pochva'].map({'má': 1, 'nemá': 0}).fillna(0)
 raw['prsten'] = raw['prsten'].map({'má': 1, 'nemá': 0}).fillna(0)
 raw['jedla'] = raw['jedla'].map({'jedlá': 1, 'nejedlá': 0}).fillna(0)
 
-# prekodovat hymenofor
+# recode hymenofor
 raw['hymenofor'] = raw['hymenofor'].replace('jiné', '-')
 
-# zmenit pomlcku na missing data
+# change dash to nan
 raw = raw.applymap(change_dash_to_nan)
 
-# zkopirovat dataframe na praci
+# copy df
 df = raw.copy()
 
-# aplikovat pomocnou funkci
+# change large str to list
 df['vyskyt_doba'] = df[['vyskyt_doba']].applymap(to_list)
 df['vyskyt_misto'] = df[['vyskyt_misto']].applymap(to_list)
 
-# binarizuje sloupce, tedy vytvori co misto, to sloupec a hodnoty 0/1
+# binarizes columns
 col_to_binarize1 = df['vyskyt_doba']
 col_to_binarize2 = df['vyskyt_misto']
 
-# vytvorit instanci binarizeru
+# instantiates binarizer
 mlb = MultiLabelBinarizer()
 
-# binarizovat mesice
+# binarize months
 df_months_binarized = pd.DataFrame(mlb.fit_transform(col_to_binarize1), columns = mlb.classes_, index = df.index)
 df_places_binarized = pd.DataFrame(mlb.fit_transform(col_to_binarize2), columns = mlb.classes_, index = df.index)
 
-# odstrani puvodni sloupce s nebinarizovanymi hodnotami
+# removes non-binarized columns
 df = df.drop(columns = ['vyskyt_doba', 'vyskyt_misto'])
 
-# merge s binarizovanymi sloupci
+# merge with binarized columns
 df = df.merge(df_months_binarized, right_index = True, left_index = True, how = 'left')
 df = df.merge(df_places_binarized, right_index = True, left_index = True, how = 'left')
 
-# list ceskych mesicu
+# list of czech months
 months_cz = ['leden', 'únor', 'březen', 'duben', 'květen', 'červen', 
              'červenec', 'srpen', 'září', 'říjen', 'listopad', 'prosinec']
 
-# dataframe s frekvencemi vyskytu mesicu
+# dataframe with months frequency
 month_freq = pd.DataFrame(df[months_cz].apply(sum, axis = 0), columns = ['month'])
 month_freq['month'] = month_freq['month'].astype(int)
 
 ############
-# druhy graf
+# (2) plot
 ############
-# list s misty vyskytu
+# list with places of occurence
 places = ['jehličnatý les', 'kosodřevina', 'křoviny',
        'listnatý les', 'louky', 'mýtiny', 'parky', 'paseky', 'pastviny',
        'pole', 'rašeliniště', 'sady', 'smíšený les', 'stráně', 'zahrady']
 
-# dataframe s frekvencemi vzhledem k mistu vyskytu
+# dataframe with places frequency
 places_freq = pd.DataFrame(df[places].apply(sum, axis = 0), columns = ['place'])
 places_freq['place'] = places_freq['place'].astype(int)
 places_freq = pd.DataFrame(places_freq['place'].sort_values(ascending = False))
 
 ###############################################################################
-# KDY ROSTOU HOUBY
+# When mushrooms grow
 ###############################################################################
 
-# parametry grafu
+# plot parameters
 size = (16.5, 9)
 fig, ax = plt.subplots(figsize=size)
 
-# vytvoreni grafu 1
+# creation
 sns.set_style('white')
 ax = sns.barplot(y="month", x=month_freq.index, data=month_freq, palette = 'Reds')
 sns.despine()
@@ -108,17 +107,17 @@ ax.set_ylabel('Počet druhů co roste', fontsize = 20)
 ax.set_title('Kdy jít houbařit?', fontsize = 25)
 ax.set_yticks(list(range(0,150, 10)))
 
-plt.savefig("Doba_rustu_houbicek.png")
+plt.savefig("images/when_mushrooms_grow.png")
 
 ###############################################################################
-# KDE ROSTOU HOUBY
+# Where mushrooms grow
 ###############################################################################
 
-# parametry grafu
+# plot parameters
 size = (16.5, 9)
 fig, ax = plt.subplots(figsize=size)
 
-# vytvoreni grafu 2
+# creation
 sns.set_style('white')
 ax = sns.barplot(y="place", x=places_freq.index, data=places_freq, color = '#006400')
 sns.despine()
@@ -128,17 +127,17 @@ ax.set_ylabel('Počet druhů co roste', fontsize = 20)
 ax.set_title('A kam jít houbařit?', fontsize = 25)
 ax.set_yticks(list(range(0,150, 10)))
 
-plt.savefig("Mista_rustu_houbicek.png")
+plt.savefig("images/where_mushrooms_grow.png")
 
 ###############################################################################
-# JAKE SBIRAT HOUBY
+# Which to pick
 ###############################################################################
 
-# parametry grafu
+# plot parameters
 size = (16.5, 12)
 fig, ax = plt.subplots(figsize=size)
 
-# samotny graf
+# creation
 sns.set_style('white')
 sns.countplot(x="hymenofor", hue="jedla", data=df, palette = 'pastel')
 sns.despine()
@@ -150,17 +149,17 @@ ax.set_title('A co sbírat?', fontsize = 20)
 plt.legend(prop={'size': 25}, labels = ['Nejedlá', 'Jedlá'])
 ax.set_yticks(list(range(0,60, 5)))
 
-plt.savefig("Jake_sbirat_houbicky.png")
+plt.savefig("images/which_to_pick.png")
 
 ###############################################################################
-# CO PRSTEN
+# Ring
 ###############################################################################
 
-# parametry grafu
+# plot parameters
 size = (16.5, 12)
 fig, ax = plt.subplots(figsize=size)
 
-# samotny graf
+# creation
 sns.set_style('white')
 sns.countplot(x="prsten", hue="jedla", data=df, palette = 'pastel')
 sns.despine()
@@ -172,4 +171,4 @@ ax.set_title('Pozor na prsteny!', fontsize = 20)
 plt.legend(prop={'size': 25}, labels = ['Nejedlá', 'Jedlá'])
 ax.set_yticks(list(range(0,90, 5)));
 
-plt.savefig("Co_houbicky_s_prstenem.png")
+plt.savefig("images/ring.png")
